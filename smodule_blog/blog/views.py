abc13679b900b53bfd24fa9402d9856519cc9975
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-01-20 08:47:33 +0600

    21 Days Challenge (Day 20)

--- smodule_blog/blog/views.py
+++ smodule_blog/blog/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author':'CoreyMS',
+        'title':'Blog Post 1',
+        'content':'First Post Content',
+        'date_posted':'august 2019'
+    },
+    {
+        'author':'Sabbir',
+        'title':'Blog Post c',
+        'content':'second Post Content',
+        'date_posted':'jan 2019'
+    }
+
+]
 
 def home(request):
-    return HttpResponse('This our home page')
+    #return HttpResponse('This our home page')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request,'blog/about.html',{'title':'About'})
