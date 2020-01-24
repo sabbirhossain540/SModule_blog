0b65a5cbab5281559a66ceacf7946670338e57e1
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-01-24 12:01:36 +0600

    Day 24

--- smodule_blog/smodule_blog/urls.py
+++ smodule_blog/smodule_blog/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_view
 from django.urls import path, include
 from users import views as user_views
 #from . import views
@@ -22,5 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('blog/', include('blog.url')),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_view.LoginView.as_view(template_name = 'users/login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
     path('', include('blog.url')),
 ]
--- smodule_blog/users/views.py
+++ smodule_blog/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 #from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
+
 
 # Create your views here.
 def register(request):
@@ -10,9 +12,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            #messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
 
     return render(request, 'users/register.html',{'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
