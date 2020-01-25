4d69984810b4689323138f1ad55814bb10bc8518
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-01-25 07:43:11 +0600

    Day 25

--- smodule_blog/smodule_blog/settings.py
+++ smodule_blog/smodule_blog/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
--- smodule_blog/smodule_blog/urls.py
+++ smodule_blog/smodule_blog/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_view
 from django.urls import path, include
 from users import views as user_views
+#For Showing Image
+from django.conf import settings
+from django.conf.urls.static import static
 #from . import views
 
 urlpatterns = [
@@ -28,3 +31,6 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
     path('', include('blog.url')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
