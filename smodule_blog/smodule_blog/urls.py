736b0674a7747ed01298203a9bae91a83b0e4cf6
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-01-31 07:37:06 +0600

    Day 31

--- smodule_blog/smodule_blog/urls.py
+++ smodule_blog/smodule_blog/urls.py
@@ -29,6 +29,12 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_view.LoginView.as_view(template_name = 'users/login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
+    path('password-reset/', auth_view.PasswordResetView.as_view(template_name = 'users/password_reset.html'),
+        name='password_reset'),
+    path('password-reset/done/', auth_view.PasswordResetDoneView.as_view(template_name = 'users/password_reset_done.html'),
+        name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>', auth_view.PasswordResetConfirmView.as_view(template_name = 'users/password_reset_confirm.html'),
+        name='password_reset'),
     path('', include('blog.url')),
 ]
 
