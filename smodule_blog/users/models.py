3c81b6aeffcb69d73ab2eea8ac669ea2e3649bed
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-01-31 11:09:42 +0600

    Day 31

--- smodule_blog/users/models.py
+++ smodule_blog/users/models.py
@@ -4,13 +4,13 @@ from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='demo.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
