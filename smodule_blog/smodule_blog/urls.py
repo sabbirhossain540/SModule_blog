c70b8935e44b665b968a7a455620b64408bc6106
Sabbir Hossain <36231104+sabbirhossain540@users.noreply.github.com>
2020-02-09 23:08:54 +0600

    Update urls.py

--- smodule_blog/smodule_blog/urls.py
+++ smodule_blog/smodule_blog/urls.py
@@ -22,6 +22,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 #from . import views
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('blog/', include('blog.url')),
